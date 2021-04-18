3074406adfb99091803b4e7a041408213d50049f
rungene <rungenesamuel@gmail.com>
2021-04-18 16:57:00 +0300

    Test Repository with Constructor Dependency Injection and DI

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -0,0 +1,53 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.core.IsEqual
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+
+class DefaultTasksRepositoryTest{
+    private val task1 = Task("Title1", "Description1")
+    private val task2 = Task("Title2", "Description2")
+    private val task3 = Task("Title3", "Description3")
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+    private lateinit var tasksRemoteDataSource: FakeDataSource
+    private lateinit var tasksLocalDataSource: FakeDataSource
+
+    // Class under test
+    private lateinit var tasksRepository: DefaultTasksRepository
+
+    @Before
+    fun createRepository(){
+        tasksRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
+        tasksLocalDataSource = FakeDataSource(localTasks.toMutableList())
+
+        tasksRepository = DefaultTasksRepository(
+                tasksRemoteDataSource,
+                tasksLocalDataSource,
+                Dispatchers.Unconfined
+        )
+    }
+
+/*    Write a test for the repository's getTasks method. Check that when you call get tasks with
+    true (meaning that it should reload from the remote data source) that it returns data from the
+    remote data source (as opposed to the local data source):  */
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun getTasks_requestsAllTasksFromRemoteDataSource() = runBlocking{
+        // When tasks are requested from the tasks repository
+        val tasks = tasksRepository.getTasks(true) as Result.Success
+
+        // Then tasks are loaded from the remote data source
+        assertThat(tasks.data, IsEqual(remoteTasks))
+    }
+}
