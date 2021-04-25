787205e25743600020d4f6a73f1eeec24d83d2b3
rungene <rungenesamuel@gmail.com>
2021-04-25 14:58:27 +0300

    StatisticsViewModelTest

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,40 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest {
+
+    // Executes each task synchronously using Architecture Components.
+    //your add instantExecutorRule since you are testing Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    //since you are testing coroutines and view models
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Subject under test
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    // Use a fake repository to be injected into the view model.
+    //test doubles for its dependencies
+    private lateinit var tasksRepository: FakeTestRepository
+
+//Create a @Before method that sets up the subject under test and dependencies.
+    @Before
+    fun setupStatisticsViewModel() {
+        // Initialise the repository with no tasks.
+        tasksRepository = FakeTestRepository()
+
+        statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+}
