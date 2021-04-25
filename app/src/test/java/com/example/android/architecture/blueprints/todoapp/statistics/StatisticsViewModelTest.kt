2520494367dc5d9a0e52043888d0aca826aff9fd
rungene <rungenesamuel@gmail.com>
2021-04-25 20:40:13 +0300

    how to write coroutine tests that use TestCoroutineDispatcher's ability to pause and resume coroutine execution. This gives you more control when writing tests that require precise timing.

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -3,10 +3,13 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.tasks.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class StatisticsViewModelTest {
@@ -36,5 +39,19 @@ class StatisticsViewModelTest {
         tasksRepository = FakeTestRepository()
 
         statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+    @Test
+    fun loadTasks_loading() {
+        // Pause dispatcher so you can verify initial values.
+        mainCoroutineRule.pauseDispatcher()
+        // Load the task in the view model.
+        statisticsViewModel.refresh()
+
+        // Then assert that the progress indicator is shown.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+        // Execute pending coroutines actions.
+        mainCoroutineRule.resumeDispatcher()
+        // Then assert that the progress indicator is hidden.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
     }
 }
